6373ef6995dc1eacc3788ea3cda2db241784951f
Luis Eduardo Garcia <xzharkonx@gmail.com>
2020-12-08 01:29:39 -0600

    Tema 1. Lambda.

---
+++ Java8/src/main/java/com/luis/java8/tema1/lambda/App.java
@@ -0,0 +1,25 @@
+package com.luis.java8.tema1.lambda;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Tema 1. Lambda.
+ *
+ */
+public class App 
+{
+	
+	public void ordenar() {
+		List<String> lista = new ArrayList<>();
+		lista.add("Luis");
+		lista.add("Eduardo");
+		lista.add("Luis Eduardo");
+		
+	}
+	
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
